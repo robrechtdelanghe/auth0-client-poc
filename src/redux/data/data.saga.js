e63b8da9603cbfc928264a288223b8140958f496
Robrecht De Langhe <rob_dl@hotmail.com>
2019-06-14 18:44:06 +0200

    Cleanup update lines saga

--- src/redux/data/data.saga.js
+++ src/redux/data/data.saga.js
@@ -1,26 +1,10 @@
-import { select, put, takeEvery } from 'redux-saga/effects';
+import {put, select, takeEvery} from 'redux-saga/effects'
 
-import {
-  ADD_LINE_START,
-  addLineError,
-  addLineSuccess, DELETE_LINE_START, deleteLineError, deleteLineSuccess,
-} from './data.actions'
-import auth0 from "auth0-js"
-import {AUTH_CONFIG} from "../auth/auth.variables"
-import {API_DELIJN, API_SOURCE_AUTH0, API_SOURCE_DELIJN} from "../../constants"
+import {ADD_LINE_START, addLineError, DELETE_LINE_START, deleteLineError, deleteLineSuccess,} from './data.actions'
+import {API_SOURCE_AUTH0, API_SOURCE_DELIJN} from "../../constants"
 import {updateLines as updateLinesDelijn} from "./data.api.delijn"
 import {updateLines as updateLinesAuth0} from "./data.api.auth0"
 
-function* addLine2({payload}) {
-  try {
-    const { auth } = yield select()
-
-    // console.log(json)
-  } catch (error) {
-    yield put(addLineError(error));
-    console.log('Error adding line', error)
-  }
-}
 function* addLine({payload}) {
   try {
     const { auth, settings } = yield select()
@@ -31,22 +15,7 @@ function* addLine({payload}) {
       subscribed: payload.subscribed || false,
     }
 
-    switch (settings.apiSource) {
-      case API_SOURCE_DELIJN: {
-        console.log('add line delijn')
-        const resultLines = yield updateLinesDelijn(auth, lines)
-        console.log(resultLines)
-        yield put(addLineSuccess(lines));
-        break;
-      }
-      case API_SOURCE_AUTH0:
-        console.log('add line auth0')
-      default: {
-        const resultLines = updateLinesAuth0(auth, lines)
-        console.log(resultLines)
-        yield put(addLineSuccess(lines));
-      }
-    }
+    yield updateLines(settings.apiSource, auth, lines)
 
   } catch (error) {
     yield put(addLineError(error));
@@ -61,22 +30,7 @@ function* deleteLine({payload}) {
     const lines = JSON.parse(JSON.stringify(auth.user.user_metadata.lines)) || {}
     delete lines[payload]
 
-    switch (settings.apiSource) {
-      case API_SOURCE_DELIJN: {
-        console.log('remove line delijn')
-        const resultLines = yield updateLinesDelijn(auth, lines)
-        console.log(resultLines)
-        yield put(deleteLineSuccess(lines));
-        break;
-      }
-      case API_SOURCE_AUTH0:
-        console.log('remove line auth0')
-      default: {
-        const resultLines = updateLinesAuth0(auth, lines)
-        console.log(resultLines)
-        yield put(deleteLineSuccess(lines));
-      }
-    }
+    yield updateLines(settings.apiSource, auth, lines)
 
   } catch (error) {
     yield put(deleteLineError(error));
@@ -84,15 +38,19 @@ function* deleteLine({payload}) {
   }
 }
 
-function promisePatchUserMedadata(management, userid, metadata) {
-  return new Promise((resolve, reject) => {
-    management.patchUserMetadata(userid, metadata, (err, data) => {
-      if (err) {
-        reject(err)
-      }
-      resolve(data)
-    })
-  })
+function* updateLines(apiSource, auth, lines) {
+  switch (apiSource) {
+    case API_SOURCE_DELIJN: {
+      const resultLines = yield updateLinesDelijn(auth, lines)
+      yield put(deleteLineSuccess(lines));
+      break;
+    }
+    case API_SOURCE_AUTH0:
+    default: {
+      const resultLines = yield updateLinesAuth0(auth, lines)
+      yield put(deleteLineSuccess(lines));
+    }
+  }
 }
 
 export function* watchAddLine() {
